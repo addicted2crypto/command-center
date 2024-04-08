fb36b672ad5bb676ef81532225eaba833e94a70b
William(addicted2crypto) <addicted2krypto@gmail.com>
2024-04-08 15:04:18 -0700

    swap chains

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -9,10 +9,10 @@ export default function Header() {
   return ( <header className='p-5 flex flex-cols bg-[#585757]'>
     
     
-    <h1 className='text-bold font-bold font-weight z-10 absolute top-3 left-3 text-[#383838] -mt-4 dark:text-[#d6d6d6]'>
+    <h1 className='text-bold font-bold font-weight z-10 absolute top-10 left-10 text-[#383838] -mt-4 dark:text-[#d6d6d6]'>
       Command Center
       </h1> 
-      <search className='pl-20 p-10 '>search accounts, NFTs, tokens...</search>
+      <search className='pl-20 p-12'>search accounts, NFTs, tokens...</search>
       <div className='p-5'>
       <Button className='bg-[#393838] rounded-lg'>Yeild yak</Button>
       </div>
@@ -22,7 +22,8 @@ export default function Header() {
       <section className='font-semibold md:pl-[30rem] sm:pl-8'>
         <image className='p-8'>profile image</image>
             <div>
-                <button className='p-3'>notifications</button>
+              <a>swap chains</a>
+                <Button className='p-3 absolute top-6 right-6'>notifications</Button>
 
             </div>
      </section>
