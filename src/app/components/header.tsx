e8729419d95af466cba19a564d525cddd9e84f06
William(addicted2crypto) <addicted2krypto@gmail.com>
2024-04-09 17:39:28 -0700

    weights

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -9,10 +9,10 @@ export default function Header() {
   return ( <header className='p-5 flex flex-cols bg-[#585757]'>
     
     
-    <h1 className='text-bold font-bold font-weight z-10 absolute top-10 left-10 text-[#383838] -mt-4 dark:text-[#d6d6d6]'>
+    <h1 className='flex flex-row text-bold font-bold font-weight z-10 absolute top-10 left-10 text-[#383838] -mt-4 dark:text-[#d6d6d6]'>
       Command Center
       </h1> 
-      <search className='pl-20 p-12'>search accounts, NFTs, tokens...</search>
+      <search className='pl-20 p-12 flex-auto'>search accounts, NFTs, tokens...</search>
       <div className='p-5'>
       <Button className='bg-[#393838] rounded-lg'>Yeild yak</Button>
       </div>
@@ -20,9 +20,9 @@ export default function Header() {
       <div className='font-bold p-5'>Bridge</div>
       <div className='font-bold p-5'>View multichain</div>
       <section className='font-semibold md:pl-[30rem] sm:pl-8'>
-        <image className='p-8'>profile image</image>
+        <image className='p-8 absolute top-6 right-[6rem]'>profile image </image>
             <div>
-              <a>swap chains</a>
+              <a href="/swap" className='p-8'>swap chains</a>
                 <Button className='p-3 absolute top-6 right-6'>notifications</Button>
 
             </div>
