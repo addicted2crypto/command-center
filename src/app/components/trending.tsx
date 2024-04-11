a7d4e5cfa68958c44200c24e143e1bce5a1f5ae2
William(addicted2crypto) <addicted2krypto@gmail.com>
2024-04-11 13:48:56 -0700

    trending arct

--- src/app/components/trending.tsx
+++ src/app/components/trending.tsx
@@ -4,7 +4,7 @@ export default function Trending() {
   return (
     <div className='text-5xl flex flex-center w-auto'>Trending and what's hot section
 
-    <p>Tool links trending protocols and chains and marketing location 1</p>
+    <p className='p-8'>Tool links trending protocols and chains and marketing location 1</p>
     </div>
   )
 }
